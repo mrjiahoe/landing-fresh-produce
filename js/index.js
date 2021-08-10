7cac56e049aa36649d42b98045d58944c03bbdb9
jiahoe <mrjiahoe@hotmail.com>
2021-08-10 14:38:11 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const openNav = document.querySelector(".open-btn");
+const closeNav = document.querySelector(".close-btn");
+const menu = document.querySelector(".nav-list");
+
+openNav.addEventListener("click", () => {
+	menu.classList.add("show");
+});
+
+closeNav.addEventListener("click", () => {
+	menu.classList.remove("show");
+});
+
+// Fix Nav
+const navBar = document.querySelector(".nav");
+const mavHeight = document.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+	const scrollHeight = window.pageYOffset;
+	if (scrollHeight > navHeight) {
+		navBar.classList.add("fix-nav");
+	} else {
+		navBar.classList.remove("fix-nav");
+	}
+});
